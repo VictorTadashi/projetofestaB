e080b11269291fe061906b78f17f32e835eea224
VictorTadashi <victortadashisaitobarra@gmail.com>
2023-10-18 09:50:16 -0300

    Senha com Ligação com MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,41 @@
 //modulos
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 //routes
+const authRouter = require('./routes/authRoutes');
 //midllewares
 
 //config
-
 const dbName = "partytime";
 const port = 3000;
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-//atrelando a primeira rota 
+//atrelando a primeira rota
+app.use("/api/auth", authRouter);
+
+
+//conexao mongodb
+mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
+    useNewUrlParser:true,
+    useUnifiedTopology : true,
+    serverSelectionTimeoutMS : 20000
+})
+
 
 app.get("/", (req, res)=>{
-        res.json({message: "Rota teste, será trocada"})
+    res.json({message : "Rota de teste, será trocada"});
 })
 
-//executar ou entender a porta 3000
-
+//escutar ou entende a porta 3000
 app.listen(port, ()=>{
-    console.log(`O banckend esta rodando na porta ${port}`)
+    console.log(`O backend está rodando na porta ${port}`)
 })
-
-
 
